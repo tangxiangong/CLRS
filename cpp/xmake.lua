c7b2deea164d1b54d4758629caee4b7dc01dd884
tangxiangong <tangxiangong@gmail.com>
2025-09-19 16:38:20 +0800

    Remove xmake hooks and reorganize project structure

--- cpp/xmake.lua
+++ cpp/xmake.lua
@@ -1,6 +1,12 @@
 add_rules("mode.debug", "mode.release")
 add_rules("plugin.compile_commands.autoupdate")
 set_languages("c++23")
+
+add_includedirs("src")
+
 target("CLRS")
     set_kind("binary")
-    add_files("src/**.cpp")
+    add_files("src/main.cpp")
+    add_headerfiles("src/utils.hpp", "src/error.hpp")
+
+includes("src/chapter2")
