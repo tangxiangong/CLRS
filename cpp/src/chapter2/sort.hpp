9570065036eb6a814cc18bc2c1f37813acd56ce1
tangxiangong <tangxiangong@gmail.com>
2025-09-20 08:51:58 +0800

    Fix unsigned integer underflow in insertion sort
    The original implementation could underflow when j (size_t) reaches 0
    and decrements. Now we explicitly handle the j=0 case.

--- cpp/src/chapter2/sort.hpp
+++ cpp/src/chapter2/sort.hpp
@@ -10,22 +10,32 @@ using std::vector;
 
 template <typename T>
 concept LessComparable = requires(const T &a, const T &b) {
-  { a < b } -> std::convertible_to<bool>;
+    { a < b } -> std::convertible_to<bool>;
 };
 
 /*
  * Insertion sort implementation.
  */
 template <LessComparable T> void insertion_sort(vector<T> &arr) {
-  for (size_t i = 1; i < arr.size(); i++) {
-    T key = arr[i];
-    int j = i - 1;
-    while (j >= 0 && key < arr[j]) {
-      arr[j + 1] = arr[j];
-      j--;
+    for (size_t i = 1; i < arr.size(); i++) {
+        T key = arr[i];
+        size_t j = i - 1;
+        bool overflow = false;
+        while (arr[j] > key) {
+            arr[j + 1] = arr[j];
+            if (j == 0) {
+                overflow = true;
+                break;
+            } else {
+                j--;
+            }
+        }
+        if (overflow) {
+            arr[0] = key;
+        } else {
+            arr[j + 1] = key;
+        }
     }
-    arr[j + 1] = key;
-  }
 }
 
 #endif // SORT_HPP
