9e23b26f2197878d53fe335ac833a862f97d6bba
tangxiangong <tangxiangong@gmail.com>
2025-10-05 14:28:30 +0800

    Add matrix multiply

--- cpp/src/chapter4/chapter4.hpp
+++ cpp/src/chapter4/chapter4.hpp
@@ -1,1 +1,31 @@
 // Chapter 4 Divide and Conquer
+
+#include "matrix.hpp"
+#include <cassert>
+
+/**
+ * MATRIX-MULTIPLY A, B, C, n
+ * ```
+ * for i = 1 to n
+ *     for j = 1 to n
+ *         for k = 1 to n
+ *             C[i, j] = C[i, j] + A[i, k] * B[k, j]
+ * ```
+ */
+template <typename T>
+    requires requires(T &&a, T &&b) {
+        { a + b } -> std::convertible_to<T>;
+        { a * b } -> std::convertible_to<T>;
+        { a += b } -> std::same_as<T&>;
+    }
+void matrix_multiply(const Matrix<T> &A, const Matrix<T> &B, Matrix<T> &C) {
+    assert(A.ncols() == B.nrows() && A.nrows() == C.nrows() &&
+           B.ncols() == C.ncols());
+    for (size_t i = 0; i < A.nrows(); ++i) {
+        for (size_t j = 0; j < B.ncols(); ++j) {
+            for (size_t k = 0; k < A.ncols(); ++k) {
+                C[i, j] += (A[i, k] * B[k, j]);
+            }
+        }
+    }
+}
--- rust/src/chapter4/mod.rs
+++ rust/src/chapter4/mod.rs
@@ -1,1 +1,30 @@
 // Chapter 4 Divide and Conquer
+
+use crate::matrix::Matrix;
+use std::ops::{Add, AddAssign, Mul};
+
+/// MATRIX-MULTIPLY A, B, C, n
+/// ```
+/// for i = 1 to n
+///     for j = 1 to n
+///         for k = 1 to n
+///             c_{i,j} = c_{i,j} + a_{i,k} * b_{k,j}
+/// ```
+///
+/// C = C + A * B
+#[allow(non_snake_case)]
+pub fn matrix_multiply<T>(A: &Matrix<T>, B: &Matrix<T>, C: &mut Matrix<T>)
+where
+    T: Copy + Add<Output = T> + Mul<Output = T> + AddAssign,
+{
+    assert_eq!(A.ncols(), B.nrows());
+    assert_eq!(A.nrows(), C.nrows());
+    assert_eq!(B.ncols(), C.ncols());
+    for i in 0..A.nrows() {
+        for j in 0..B.ncols() {
+            for k in 0..A.ncols() {
+                C[(i, j)] += A[(i, k)] * B[(k, j)];
+            }
+        }
+    }
+}
